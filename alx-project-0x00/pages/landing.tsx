67f4fd47d66da4232f45f7636957b35c552b004c
Elleni-Bisrat <ellenibisrat16@gmail.com>
2025-09-26 10:22:12 +0300

    update

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -8,9 +8,10 @@ const Landing: React.FC = () => {
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
       <div className="mt-6 flex gap-4 flex-wrap">
-        <Button title="Small Rounded" styles="text-sm rounded-sm" />
-        <Button title="Small Medium" styles="text-sm rounded-md" />
-        <Button title="Small Full" styles="text-sm rounded-full" />
+         <Button title="Small Button" styles="rounded-sm" />
+      <Button title="Medium Button" styles="rounded-md" />
+      <Button title="Full Button" styles="rounded-full" />
+      <Button title="Large Rounded" styles="rounded-lg" />
       </div>
     </div>
   );
