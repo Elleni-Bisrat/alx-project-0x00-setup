a0d0fca23ecac87a8e1302e6dc1537ff75e19151
Elleni-Bisrat <ellenibisrat16@gmail.com>
2025-09-26 10:13:37 +0300

    advanced task

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,8 +1,18 @@
-const Landing: React.FC =  () => {
+import React from "react";
+import Card from "@/components/Card";
+import Button from "@/components/Button";
+
+const Landing: React.FC = () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
+      <Card />
+      <div className="mt-6 flex gap-4 flex-wrap">
+        <Button title="Small Rounded" styles="text-sm rounded-sm" />
+        <Button title="Small Medium" styles="text-sm rounded-md" />
+        <Button title="Small Full" styles="text-sm rounded-full" />
+      </div>
     </div>
-  )
-}
-export default Landing
+  );
+};
+export default Landing;
